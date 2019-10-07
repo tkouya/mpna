002824723180e67319101915ff384689dde54482
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-10-07 21:33:49 +0900

    Add files via upload

---
+++ logistic.c
@@ -0,0 +1,37 @@
+//******************************************************************************
+// logistic.c : Sequence calculation based on logistic map
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//******************************************************************************
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	double x[102];
+
+	// Initial value of the sequence
+	x[0] = 0.7501;
+
+	for(i = 0; i <= 100; i++)
+	{
+		x[i + 1] = 4 * x[i] * (1 - x[i]);
+		if((i % 10) == 0)
+			printf("%5d, %25.17e\n", i, x[i]);
+	}
+
+	return 0;
+}
