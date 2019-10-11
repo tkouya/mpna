7622d55e2425562b75bc8dff2485d52637eb913f
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-10-11 13:55:20 +0900

    Add files via upload

--- test_iterative_ref_qd.cpp
+++ test_iterative_ref_qd.cpp
@@ -1,10 +1,22 @@
-/**********************************************************/
-/* Iterative Refinement Methods with various precision    */
-/*                                                        */
-/* Copyright (c) 2018 Tomonori Kouya, All rights reserved */
-/*                                                        */
-/* Version 0.0: 2018-12-15 (Tue) First published          */
-/**********************************************************/
+//******************************************************************************
+// test_iterative_ref_qd.cpp : Interative refinement method based 
+//                               on direct method (double and dd_real + qd_real)
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 
@@ -17,7 +29,7 @@
 #include "qd/fpu.h"
 
 // Template linear compucation with double, QD, MPFR/GMP
-#include "template_linear_complete.h"
+#include "template_linear.h"
 
 // Time routines
 #include "get_secv.h"
