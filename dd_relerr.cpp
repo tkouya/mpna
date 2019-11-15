f7604a466027a99b1c1991ea3eac5ae4f34dacdd
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 20:25:12 +0900

    Rewrite comments

--- dd_relerr.cpp
+++ dd_relerr.cpp
@@ -1,3 +1,21 @@
+//******************************************************************************
+// dd_relerr.cpp : Sample code to get relative error of dd_real variables
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 
