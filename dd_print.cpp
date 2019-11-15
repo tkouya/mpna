11359041ffc8a758ec9007b57ad424dd251dcbc4
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 20:26:28 +0900

    Rewrite comments

--- dd_print.cpp
+++ dd_print.cpp
@@ -1,21 +1,21 @@
-/********************************************************************************/
-/* dd_test.cc:Double-double and Quadruple precision Linear Computation Library  */
-/* Copyright (C) 2015-2016 Tomonori Kouya                                       */
-/*                                                                              */
-/* This program is free software: you can redistribute it and/or modify it      */
-/* under the terms of the GNU Lesser General Public License as published by the */
-/* Free Software Foundation, either version 3 of the License or any later       */
-/* version.                                                                     */
-/*                                                                              */
-/* This program is distributed in the hope that it will be useful, but WITHOUT  */
-/* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        */
-/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License */
-/* for more details.                                                            */
-/*                                                                              */
-/* You should have received a copy of the GNU Lesser General Public License     */
-/* along with this program.  If not, see <http://www.gnu.org/licenses/>.        */
-/*                                                                              */
-/********************************************************************************/
+//******************************************************************************
+// dd_print.cpp : Test program to print double-double precision variables
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 
