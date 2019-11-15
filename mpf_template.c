4cbff57db09c40c1ab69aa8054d88ea9080976b5
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 19:17:20 +0900

    Rewrite comments

--- mpf_template.c
+++ mpf_template.c
@@ -1,41 +1,57 @@
-// mpf_template.c: mpf_t型の使い方
-
+//******************************************************************************
+// mpf_template.c : Sample code of mpf_t variables
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <stdio.h>
 #include "gmp.h"
 
 int main(void)
 {
 	unsigned long prec;
-	mpf_t a, b, c; // デフォルトの精度
-	mpf_t ad, bd, cd; // a, b, cより長い桁
+	mpf_t a, b, c; // default precision
+	mpf_t ad, bd, cd; // larger precsion of a, b and c
 
-	printf("Input default prec in bits: "); scanf("%ld", &prec);
+	printf("Input default prec in bits: "); while(scanf("%ld", &prec) < 1);
 
-	// デフォルトの仮数部ビット数をセット
+	// set default precision
 	mpf_set_default_prec((mp_bitcnt_t)prec); // in bits
 
-	// デフォルトの精度を表示
+	// print default precision
 	printf("default prec in bits: %ld\n", prec);
 
-	mpf_init(a); // デフォルト精度
-	mpf_inits(b, c, NULL); // デフォルト精度でまとめて初期化
+	mpf_init(a); // initialize with default precision
+	mpf_inits(b, c, NULL); // multiple initialization with default precision
 
-	// デフォルト仮数部の2倍に設定
+	// initialize with twice precsion
 	mpf_init2(ad, (mp_bitcnt_t)(prec * 2));
 	mpf_init2(bd, (mp_bitcnt_t)(prec * 2));
 	mpf_init2(cd, (mp_bitcnt_t)(prec * 2));
 
-	// 変数ごとの仮数部ビット数を表示
+	// print precision of ad
 	printf("prec(ad) = %ld\n", mpf_get_prec(ad));
 
-	// ・・・・・・・・・・
-	// ・・・演算処理・・・
-	// ・・・・・・・・・・
+	// ---------------------
+	// variable calculations
+	// ---------------------
 
-	// 変数消去
-	mpf_clear(a); // 単独
+	// clear one variable
+	mpf_clear(a);
 
-	// まとめて消去
+	// clear more than one variables
 	mpf_clears(b, c, NULL);       
 	mpf_clears(ad, bd, cd, NULL); 
 
--- mpf_template.cpp
+++ mpf_template.cpp
@@ -1,5 +1,21 @@
-// mpf_template.cpp: mpf_class型の使い方
-
+//******************************************************************************
+// mpf_template.cpp : Sample code of mpf_class variables
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 #include "gmpxx.h"
@@ -12,33 +28,33 @@ int main(void)
 
 	cout << "Input default prec in bits: "; cin >> prec;
 
-	// デフォルトの仮数部ビット数をセット
+	// set default precision
 	mpf_set_default_prec((mp_bitcnt_t)prec); // in bits
 
-	mpf_class a, b, c; // デフォルトの精度
-	mpf_class ad, bd, cd; // a, b, cより長い桁
+	mpf_class a, b, c; // default precision
+	mpf_class ad, bd, cd; // default precision
 
-	// デフォルトの精度を表示
+	// print default precision
 	cout << "default prec in bits: " << prec << endl;
 
-	// デフォルト仮数部の2倍に設定
+	// change twice precision for ad, bd, and cd
 	ad.set_prec((mp_bitcnt_t)(prec * 2));
 	bd.set_prec((mp_bitcnt_t)(prec * 2));
 	cd.set_prec((mp_bitcnt_t)(prec * 2));
 
-	// 変数ごとの仮数部ビット数を表示
+	// print each precision of variable
 	cout << "prec(ad) = " << ad.get_prec() << endl;
 
-	// ・・・・・・・・・・
-	// ・・・演算処理・・・
-	// ・・・・・・・・・・
+	//----------------------
+	// calculations
+	//----------------------
 
-	// 変数消去
-	~a; // 単独
+	// delete one variable
+	delete a; // single
 
-	// まとめて消去
-	~b, ~c;
-	~ad, ~bd, ~cd; 
+	// delete variables
+	delete b, c;
+	delete ad, bd, cd;
 
 	return 0;
 }
