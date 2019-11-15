3c45c3dc902e1d4ebaea6c1400912d2286fbc965
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 18:55:49 +0900

    Rewrite comments

--- test_mpfi.c
+++ test_mpfi.c
@@ -1,7 +1,24 @@
-/* MPFI test program */
+//******************************************************************************
+// test_mpfi.c : Test program of MPFI
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <stdio.h>
 
-#include "mpfr.h"
+//#include "mpfr.h"
 #include "mpfi.h"
 
 int main()
@@ -9,12 +26,10 @@ int main()
 	unsigned long prec;
 	mpfi_t a, b, c;
 
-	//prec = 256;
-
 	printf("prec, a, b, c\n");
 	for(prec = 16; prec <= 1024; prec *= 2)
 	{
-		// ‰Šú‰»
+		// Initialize all variables
 		mpfi_init2(a, prec);
 		mpfi_init2(b, prec);
 		mpfi_init2(c, prec);
@@ -24,15 +39,15 @@ int main()
 		mpfi_set_ui(b, 3UL); mpfi_sqrt(b, b);
 		mpfi_set_ui(c, 5UL); mpfi_sqrt(c, c);
 
-		// o—Í
+		// Print intervals
 		//printf("a = ");
-		 mpfi_out_str(stdout, 10, 10, a); printf(", "); //printf("\n");
+		mpfi_out_str(stdout, 10, 10, a); printf(", "); //printf("\n");
 		//printf("b = ");
-		 mpfi_out_str(stdout, 10, 10, b); printf(", "); //printf("\n");
+		mpfi_out_str(stdout, 10, 10, b); printf(", "); //printf("\n");
 		//printf("c = ");
-		 mpfi_out_str(stdout, 10, 10, c); printf("\n");
+		mpfi_out_str(stdout, 10, 10, c); printf("\n");
 
-		// Á‹Ž
+		// Clear all variables
 		mpfi_clear(a);
 		mpfi_clear(b);
 		mpfi_clear(c);
--- test_quadmath.c
+++ test_quadmath.c
@@ -1,3 +1,21 @@
+//******************************************************************************
+// test_quadmath.c : Test program of quadmath library with GCC
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <stdio.h>
 #include <quadmath.h>
 
@@ -21,4 +39,3 @@ int main()
 
 	return 0;
 }
-
