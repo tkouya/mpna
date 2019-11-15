5b5b9ee466760c35e7f25726c065137327f87634
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 20:24:23 +0900

    Rewrite comments

--- dd_test.cpp
+++ dd_test.cpp
@@ -1,21 +1,21 @@
-/********************************************************************************/
-/* dd_test.cc:Double-double and Quadruple precision Linear Computation Library  */
-/* Copyright (C) 2015-2016 Tomonori Kouya                                       */
-/*                                                                              */
-/* This program is free software: you can redistribute it and/or modify it      */
-/* under the terms of the GNU Lesser General Public License as published by the */
-/* Free Software Foundation, either version 3 of the License or any later       */
-/* version.                                                                     */
-/*                                                                              */
-/* This program is distributed in the hope that it will be useful, but WITHOUT  */
-/* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        */
-/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License */
-/* for more details.                                                            */
-/*                                                                              */
-/* You should have received a copy of the GNU Lesser General Public License     */
-/* along with this program.  If not, see <http://www.gnu.org/licenses/>.        */
-/*                                                                              */
-/********************************************************************************/
+//******************************************************************************
+// dd_test.cpp : Double-double precision computation with QD library
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 
@@ -36,6 +36,9 @@ int main()
 
 	dd_a = sqrt((dd_real)2) * 2;
 	dd_b = sqrt((dd_real)2);
+
+	printf("dd_a[0] = %25.17e\n", dd_a.x[0]);
+	printf("dd_a[1] = %25.17e\n", dd_a.x[1]);
 
 	cout << "DD decimal digigs = " << dd_real::_ndigits << endl;
 
