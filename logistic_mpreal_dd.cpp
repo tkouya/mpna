802f018e93e4b06b84aa5ee18170838d41516dbf
Tomonori Kouya <tkouya@users.noreply.github.com>
2019-11-15 20:05:24 +0900

    Rewrite comments

--- logistic_mpreal_dd.cpp
+++ logistic_mpreal_dd.cpp
@@ -1,4 +1,21 @@
-// logistic 写像
+//******************************************************************************
+// logistic_dd.cpp : Sequence calculation based on logistic map 
+// Copyright (C) 2019 Tomonori Kouya
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License or any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//******************************************************************************
 #include <iostream>
 #include <iomanip>
 
@@ -11,315 +28,21 @@
 using namespace std;
 using namespace mpfr;
 
-
-// based on get_ld.c code in MPFR 4.0.0 
-#define MPFR_EXP_ZERO (mpfr_get_emin() + 1)
-#define MPFR_EXP_NAN  (mpfr_get_emin() + 2)
-#define MPFR_EXP_INF  (mpfr_get_emin() + 3)
-#define MPFR_EXP_UBF  (mpfr_get_emin() + 4)
-
-// FALSE(=0), TRUE(=1)
-//#define MPFR_IS_SINGULAR(x) (MPFR_EXP(x) <= MPFR_EXP_INF)
-int mpfr_is_singular(mpfr_srcptr x)
-{
-	return (mpfr_get_exp(x) <= MPFR_EXP_INF);
-}
-
-/* generic code */
-// ret_dd[2] = ret_dd[high == 0], ret_dd[low == 1]
-void mpfr_get_dd (double ret_dd[2], mpfr_srcptr x, mpfr_rnd_t rnd_mode)
-{
-	//if (MPFR_UNLIKELY (MPFR_IS_SINGULAR (x)))
-	if (mpfr_is_singular(x))
-	{
-		//return (long double) mpfr_get_d (x, rnd_mode);
-		ret_dd[0] = mpfr_get_d(x, rnd_mode);
-		ret_dd[1] = (double)0.0;
-	}
-  else /* now x is a normal non-zero number */
-	{
-      // long double r; /* result */
-      double s; /* part of result */
-      //MPFR_SAVE_EXPO_DECL (expo);
-
-      //MPFR_SAVE_EXPO_MARK (expo);
-
-//#if defined(HAVE_LDOUBLE_MAYBE_DOUBLE_DOUBLE)
-      //if (MPFR_LDBL_MANT_DIG == 106)
-      //{
-          /* Assume double-double format (as found with the PowerPC ABI).
-             The generic code below isn't used because numbers with
-             precision > 106 would not be supported. */
-          s = mpfr_get_d (x, MPFR_RNDN); /* high part of x */
-          /* Let's first consider special cases separately. The test for
-             infinity is really needed to avoid a NaN result. The test
-             for NaN is mainly for optimization. The test for 0 is useful
-             to get the correct sign (assuming mpfr_get_d supports signed
-             zeros on the implementation). */
-          //if (s == 0 || DOUBLE_ISNAN (s) || DOUBLE_ISINF (s))
-          if (s == 0 || isnan(s) || isinf(s))
-					{
-            // r = (long double) s;
-						ret_dd[0] = s;
-						ret_dd[1] = 0;
-					}
-          else
-					{
-              mpfr_t y, z;
-
-              mpfr_init2 (y, mpfr_get_prec (x));
-              mpfr_init2 (z, 53); /* keep the precision small */
-              mpfr_set_d (z, s, MPFR_RNDN);  /* exact */
-
-							// y := x - z = x - s
-              mpfr_sub (y, x, z, MPFR_RNDN); /* exact */
-
-              /* Add the second part of y (in the correct rounding mode). */
-              //r = (long double) s + (long double) mpfr_get_d (y, rnd_mode);
-							ret_dd[0] = s;
-							ret_dd[1] = mpfr_get_d(y, rnd_mode);
-
-              mpfr_clear (z);
-              mpfr_clear (y);
-          }
-      //}
-      //MPFR_SAVE_EXPO_FREE (expo);
-      //return r;
-    }
-}
-
-/* double-double code */
-int mpfr_set_dd (mpfr_ptr r, double d[2], mpfr_rnd_t rnd_mode)
-{
-  mpfr_t t, u;
-  int inexact, shift_exp;
-  double h, l;
-//  MPFR_SAVE_EXPO_DECL (expo);
-
-  /* Check for NAN */
-  //ONGDOUBLE_NAN_ACTION (d, goto nan);
-	if(isnan(d[0])) goto nan;
-
-  /* Check for INF */
-//  if (d > MPFR_LDBL_MAX)
-  if (d[0] > DBL_MAX)
-    {
-      mpfr_set_inf (r, 1);
-      return 0;
-    }
-  //else if (d < -MPFR_LDBL_MAX)
-  else if (d[0] < -DBL_MAX)
-    {
-      mpfr_set_inf (r, -1);
-      return 0;
-    }
-  /* Check for ZERO */
-  else if (d[0] == 0.0)
-    return mpfr_set_d (r, (double) d[0], rnd_mode);
-
- // if (d >= (long double) MPFR_LDBL_MAX || d <= (long double) -MPFR_LDBL_MAX)
- //   h = (d >= (long double) MPFR_LDBL_MAX) ? MPFR_LDBL_MAX : -MPFR_LDBL_MAX;
-  if (d[0] >= DBL_MAX || d[0] <= -DBL_MAX)
-    h = (d[0] >= DBL_MAX) ? DBL_MAX : -DBL_MAX;
-  else
-    h = (double) d[0]; /* should not overflow */
-  //l = (double) (d - (long double) h);
-	l = d[1];
-
-  //MPFR_SAVE_EXPO_MARK (expo);
-
-  mpfr_init2 (t, 53);
-  mpfr_init2 (u, 53);
-
-  inexact = mpfr_set_d (t, h, MPFR_RNDN);
-  //MPFR_ASSERTN(inexact == 0);
-  inexact = mpfr_set_d (u, l, MPFR_RNDN);
-  //MPFR_ASSERTN(inexact == 0);
-  inexact = mpfr_add (r, t, u, rnd_mode);
-
-  mpfr_clear (t);
-  mpfr_clear (u);
-
-//  MPFR_SAVE_EXPO_FREE (expo);
-  inexact = mpfr_check_range (r, inexact, rnd_mode);
-  return inexact;
-
- nan:
-  mpfr_set_nan(r); //MPFR_SET_NAN(r);
-  //return MPFR_RET_NAN;
-	return 0;
-}
-
-
-/* generic code */
-// ret_qd[4] = ret_qd[high == 0], ret_qd[1], ret_qd[2], ret_qd[3]
-void mpfr_get_qd (double ret_qd[4], mpfr_srcptr x, mpfr_rnd_t rnd_mode)
-{
-	//if (MPFR_UNLIKELY (MPFR_IS_SINGULAR (x)))
-	if (mpfr_is_singular(x))
-	{
-		//return (long double) mpfr_get_d (x, rnd_mode);
-		ret_qd[0] = mpfr_get_d(x, rnd_mode);
-		ret_qd[1] = (double)0.0;
-		ret_qd[2] = (double)0.0;
-		ret_qd[3] = (double)0.0;
-	}
-  else /* now x is a normal non-zero number */
-	{
-      // long double r; /* result */
-      double s; /* part of result */
-      //MPFR_SAVE_EXPO_DECL (expo);
-
-      //MPFR_SAVE_EXPO_MARK (expo);
-
-//#if defined(HAVE_LDOUBLE_MAYBE_DOUBLE_DOUBLE)
-      //if (MPFR_LDBL_MANT_DIG == 106)
-      //{
-          /* Assume double-double format (as found with the PowerPC ABI).
-             The generic code below isn't used because numbers with
-             precision > 106 would not be supported. */
-          s = mpfr_get_d (x, MPFR_RNDN); /* high part of x */
-          /* Let's first consider special cases separately. The test for
-             infinity is really needed to avoid a NaN result. The test
-             for NaN is mainly for optimization. The test for 0 is useful
-             to get the correct sign (assuming mpfr_get_d supports signed
-             zeros on the implementation). */
-          //if (s == 0 || DOUBLE_ISNAN (s) || DOUBLE_ISINF (s))
-          if (s == 0 || isnan(s) || isinf(s))
-					{
-            // r = (long double) s;
-						ret_qd[0] = s;
-						ret_qd[1] = 0;
-						ret_qd[1] = 0;
-						ret_qd[1] = 0;
-					}
-          else
-					{
-              mpfr_t y, z[3];
-
-              mpfr_init2 (y, mpfr_get_prec (x));
-              mpfr_init2 (z[0], 53); /* keep the precision small */
-              mpfr_init2 (z[1], 53); /* keep the precision small */
-              mpfr_init2 (z[2], 53); /* keep the precision small */
-
-							// y[0] := x - z = x - s
-              mpfr_set_d (z[0], s, MPFR_RNDN);  /* exact */
-              mpfr_sub (y, x, z[0], MPFR_RNDN); /* exact */
-							ret_qd[0] = s;
-
-							// y[1] := y[0] - z = x - s
- 		          s = mpfr_get_d (y, MPFR_RNDN); /* high part of y */
-	            mpfr_set_d (z[1], s, MPFR_RNDN);  /* exact */
-              mpfr_sub (y, x, z[0], MPFR_RNDN); /* exact */
-              mpfr_sub (y, y, z[1], MPFR_RNDN); /* exact */
-							ret_qd[1] = s;
-
-							// y[2] := y[1] - z = x - s
- 		          s = mpfr_get_d (y, MPFR_RNDN); /* high part of y */
-	            mpfr_set_d (z[2], s, MPFR_RNDN);  /* exact */
-              mpfr_sub (y, x, z[0], MPFR_RNDN); /* exact */
-              mpfr_sub (y, y, z[1], MPFR_RNDN); /* exact */
-              mpfr_sub (y, y, z[2], MPFR_RNDN); /* exact */
-							ret_qd[2] = s;
-
-							// y[3] := x - z = x - s
-							ret_qd[3] = mpfr_get_d(y, rnd_mode);
-
-              mpfr_clear (z[0]);
-              mpfr_clear (z[1]);
-              mpfr_clear (z[2]);
-              mpfr_clear (y);
-          }
-      //}
-      //MPFR_SAVE_EXPO_FREE (expo);
-      //return r;
-    }
-}
-
-/* double-double code */
-int mpfr_set_qd (mpfr_ptr r, double d[4], mpfr_rnd_t rnd_mode)
-{
-  mpfr_t t, u, v, w;
-  int inexact, shift_exp;
-  double h, l[3];
-//  MPFR_SAVE_EXPO_DECL (expo);
-
-  /* Check for NAN */
-  //ONGDOUBLE_NAN_ACTION (d, goto nan);
-	if(isnan(d[0])) goto nan;
-
-  /* Check for INF */
-//  if (d > MPFR_LDBL_MAX)
-  if (d[0] > DBL_MAX)
-    {
-      mpfr_set_inf (r, 1);
-      return 0;
-    }
-  //else if (d < -MPFR_LDBL_MAX)
-  else if (d[0] < -DBL_MAX)
-    {
-      mpfr_set_inf (r, -1);
-      return 0;
-    }
-  /* Check for ZERO */
-  else if (d[0] == 0.0)
-    return mpfr_set_d (r, (double) d[0], rnd_mode);
-
- // if (d >= (long double) MPFR_LDBL_MAX || d <= (long double) -MPFR_LDBL_MAX)
- //   h = (d >= (long double) MPFR_LDBL_MAX) ? MPFR_LDBL_MAX : -MPFR_LDBL_MAX;
-  if (d[0] >= DBL_MAX || d[0] <= -DBL_MAX)
-    h = (d[0] >= DBL_MAX) ? DBL_MAX : -DBL_MAX;
-  else
-    h = (double) d[0]; /* should not overflow */
-  //l = (double) (d - (long double) h);
-	l[0] = d[1];
-	l[1] = d[2];
-	l[2] = d[3];
-
-  //MPFR_SAVE_EXPO_MARK (expo);
-
-  mpfr_init2 (t, 53);
-  mpfr_init2 (u, 53);
-  mpfr_init2 (v, 53);
-  mpfr_init2 (w, 53);
-
-  inexact = mpfr_set_d (t, h, MPFR_RNDN);
-  inexact = mpfr_set_d (u, l[0], MPFR_RNDN);
-  inexact = mpfr_set_d (v, l[1], MPFR_RNDN);
-  inexact = mpfr_set_d (w, l[2], MPFR_RNDN);
-
-  inexact = mpfr_add (r, t, u, rnd_mode);
-  inexact = mpfr_add (r, v, w, rnd_mode);
-
-  mpfr_clear (t);
-  mpfr_clear (u);
-  mpfr_clear (v);
-  mpfr_clear (w);
-
-//  MPFR_SAVE_EXPO_FREE (expo);
-  inexact = mpfr_check_range (r, inexact, rnd_mode);
-  return inexact;
-
- nan:
-  mpfr_set_nan(r); //MPFR_SET_NAN(r);
-  //return MPFR_RET_NAN;
-	return 0;
-}
-
+#include "mpfr_dd_qd.h"
 
 int main(int argc, char *argv[])
 {
 	int i;
 	int num_bits, num_decimal;
 
-	// 引数チェック
+	// Check arguments
 	if(argc <= 1)
 	{
 		cerr << "Usage: " << argv[0] << " [num_bits]" << endl;
 		return 0;
 	}
 
-	// 計算桁数設定
+	// Set a default precision
 	num_bits = atoi(argv[1]);
 	if(num_bits <= 24)
 		num_bits = 24;
@@ -335,14 +58,14 @@ int main(int argc, char *argv[])
 	mpreal long_x[102]; // longer
 	double mpfr_dd[2], mpfr_qd[4];
 
-	// 2倍の桁数で初期化
+	// long_x is set as twice bits than default prec
 	for(i = 0; i < 102; i++)
 		long_x[i].set_prec(num_bits * 2);
 
 	// fix FPU mode for QD
 	fpu_fix_start(NULL);
 
-	// 初期値
+	// Initial values
 	x[0] = "0.7501";
 	dd_x[0] = "0.7501";
 	qd_x[0] = "0.7501";
